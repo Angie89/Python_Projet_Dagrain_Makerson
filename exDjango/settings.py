49648668db0f5a421ab3799f04e54281d280291b
Dagrain Michel-Ange <angiebrooke4@gmail.com>
2014-03-08 00:33:52 -0800

    my django app

--- exDjango/settings.py
+++ exDjango/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -99,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/TEMPLATES/static/'
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
@@ -117,5 +118,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    "exDjango/TEMPLATES/static"
+    ##"exDjango/TEMPLATES/static"
 )
