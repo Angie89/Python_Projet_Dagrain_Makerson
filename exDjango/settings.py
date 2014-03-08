64ebdf38401783a0601f27f3a2b069f46433ab51
Dagrain Michel-Ange <angiebrooke4@gmail.com>
2014-03-08 00:42:50 -0800

    my django app

--- exDjango/settings.py
+++ exDjango/settings.py
@@ -117,6 +117,6 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    ##os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
     'exDjango/TEMPLATES/static',
 )
